c475b355509398e32f7260ce50b769824e94597d
Миляуша <milallina_0905@icloud.com>
2024-04-17 10:30:30 +0500

    исправила

--- src/Calculator1/Calculate.java
+++ src/Calculator1/Calculate.java
@@ -6,53 +6,67 @@ import java.util.Scanner;
 public class Calculate {
     public static void main(String[] args) throws Exception {
         Scanner con = new Scanner(System.in);
-        String[] value = con.nextLine().split(" ");
+
+        System.out.println(calc(con.nextLine()));
+    }
+
+
+    public static String calc(String input) throws Exception {
+        String[] value = input.split(" ");
+
+        if (value.length > 3) {
+            throw new Exception("Введите только два числа");
+        }
+
 
         try {
             int result;
             int operand1 = romanToArabic(value[0]);
             int operand2 = romanToArabic(value[2]);
+            if (operand1 < 0 || operand2 < 0) {
+                throw new Exception("Введите положительное число");
+            }
             if (value[1].equals("+")) {
                 result = operand1 + operand2;
-                System.out.println(arabicToRoman(result));
+               return arabicToRoman(result);
             } else if (value[1].equals("-")) {
                 result = operand1 - operand2;
-                System.out.println(arabicToRoman(result));
+                return arabicToRoman(result);
             } else if (value[1].equals("*")) {
                 result = operand1 * operand2;
-                System.out.println(arabicToRoman(result));
+                return arabicToRoman(result);
             } else if (value[1].equals("/")) {
                 result = operand1 / operand2;
-                System.out.println(arabicToRoman(result));
-            }else {
-                throw new Exception();
-
-            }
-        }catch (RuntimeException ex){
-            try {int operand1 = Integer.parseInt(value[0]);
-            int operand2 = Integer.parseInt(value[2]);
-            int result2;
-            if (value[1].equals("+")) {
-                result2 = operand1 + operand2;
-                System.out.println((result2));
-            } else if (value[1].equals("-")) {
-                result2 = operand1 - operand2;
-                System.out.println((result2));
-            } else if (value[1].equals("*")) {
-                result2 = operand1 * operand2;
-                System.out.println(result2);
-            } else if (value[1].equals("/")) {
-                result2 = operand1 / operand2;
-                System.out.println((result2));
+                return arabicToRoman(result);
             } else {
                 throw new Exception();
-
             }
-        }catch (NumberFormatException num){
+        } catch (RuntimeException ex) {
+            try {
+                int operand1 = Integer.parseInt(value[0]);
+                int operand2 = Integer.parseInt(value[2]);
+                if (operand1 < 0 || operand2 < 0) {
+                    throw new Exception("Введите положительное число");
+                }
+                int result2;
+                if (value[1].equals("+")) {
+                   return Integer.toString(operand1 + operand2);
+                } else if (value[1].equals("-")) {
+                    return Integer.toString(operand1 - operand2);
+                } else if (value[1].equals("*")) {
+                    return Integer.toString(operand1 * operand2);
+                } else if (value[1].equals("/")) {
+                    return Integer.toString(operand1 / operand2);
+                } else {
+                    throw new Exception();
+                }
+
+            } catch (NumberFormatException num) {
                 throw new Exception();
             }
+        }
     }
-    }
+
     public static int romanToArabic(String value) {
         if (value.equals("I")) {
             return 1;
@@ -74,38 +88,40 @@ public class Calculate {
             return 9;
         } else if (value.equals("X")) {
             return 10;
-        }throw new RuntimeException(); //это непроверенное исключение в Java, которое возникает во время выполнения приложения.
+        }
+        throw new RuntimeException(); //это непроверенное исключение в Java, которое возникает во время выполнения приложения.
     }
-    public static String arabicToRoman(int value)  {
-        if (value == 0){
+
+    public static String arabicToRoman(int value) {
+        if (value == 0) {
             throw new RuntimeException();
         }
         String romRez = "";
         String[] roman = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C"};
         int[] arabic = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100};
-        ArrayList<Integer>numbers = new ArrayList<>();
-        while(value / 10 > 0){
-            numbers.add(value%10);
-            value = value/10;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (value / 10 > 0) {
+            numbers.add(value % 10);
+            value = value / 10;
         }
         numbers.add(value);
-        for(int i = numbers.size()-1; i>=0; i--){
+        for (int i = numbers.size() - 1; i >= 0; i--) {
             int num = (int) (numbers.get(i) * Math.pow(10, i));
             if (num == 0) {
                 continue;
             }
-            for(int j = 0; j < arabic.length; j++){
-                if(num == arabic[j]){
+            for (int j = 0; j < arabic.length; j++) {
+                if (num == arabic[j]) {
                     romRez += roman[j];
                     break;
                 } else if (num < arabic[j]) {
-                    romRez += roman[j-1];
-                    num = num - arabic[j-1];
+                    romRez += roman[j - 1];
+                    num = num - arabic[j - 1];
                     j = -1;
                 }
             }
 
         }
-       return romRez;
+        return romRez;
     }
 }
